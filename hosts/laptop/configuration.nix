1589fdb1881c38ea0415cc831bd613fe54e07a42
NolanBlack <nblackinbox@gmail.com>
2024-09-03 16:37:50 -0600

    initial commit

---
+++ hosts/laptop/configuration.nix
@@ -0,0 +1,241 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{ config, pkgs, inputs, ... }:
+
+{
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+      inputs.home-manager.nixosModules.default
+    ];
+
+  # Bootloader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  networking.hostName = "nixos"; # Define your hostname.
+  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+
+  # Configure network proxy if necessary
+  # networking.proxy.default = "http://user:password@proxy:port/";
+  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+
+  # Enable networking
+  networking.networkmanager.enable = true;
+
+  # Set your time zone.
+  time.timeZone = "America/Denver";
+
+  # Select internationalisation properties.
+  i18n.defaultLocale = "en_US.UTF-8";
+
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "en_US.UTF-8";
+    LC_IDENTIFICATION = "en_US.UTF-8";
+    LC_MEASUREMENT = "en_US.UTF-8";
+    LC_MONETARY = "en_US.UTF-8";
+    LC_NAME = "en_US.UTF-8";
+    LC_NUMERIC = "en_US.UTF-8";
+    LC_PAPER = "en_US.UTF-8";
+    LC_TELEPHONE = "en_US.UTF-8";
+    LC_TIME = "en_US.UTF-8";
+  };
+
+  # Enable the X11 windowing system.
+  services.xserver.enable = true;
+
+  # Enable the GNOME Desktop Environment.
+  services.xserver.displayManager.gdm.enable = true;
+  services.xserver.desktopManager.gnome.enable = false;
+
+  # Configure keymap in X11
+  services.xserver.autoRepeatDelay = 250;
+  services.xserver.autoRepeatInterval = 60;
+  services.xserver.xkb = {
+    layout = "us";
+    variant = "";
+    options = "caps:swapescape";
+  };
+  console.useXkbConfig = true;
+
+  # services.xserver.displayManager.sessionCommands = ''
+  #     ${pkgs.xorg.xset}/bin/xset r rate 250 60
+  # '';
+  # xsession.initExtra = "xset r rate 250 60";
+
+  
+
+  # Enable CUPS to print documents.
+  services.printing.enable = true;
+
+  # Enable sound with pipewire.
+  hardware.pulseaudio.enable = false;
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+    # If you want to use JACK applications, uncomment this
+    #jack.enable = true;
+
+    # use the example session manager (no others are packaged yet so this is enabled by default,
+    # no need to redefine it in your config for now)
+    #media-session.enable = true;
+  };
+
+  # Enable touchpad support (enabled default in most desktopManager).
+  # services.xserver.libinput.enable = true;
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.nolan = {
+    isNormalUser = true;
+    description = "nolan";
+    extraGroups = [ "networkmanager" "wheel" ];
+    packages = with pkgs; [
+    #  thunderbird
+    ];
+  };
+
+  # Install firefox.
+  programs.firefox.enable = true;
+
+  # Allow unfree packages
+  nixpkgs.config.allowUnfree = true;
+
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+  environment.systemPackages = with pkgs; [
+    vim
+    git
+    wget
+    xorg.xmodmap
+    xorg.xkbcomp
+    xorg.xset
+    #hyprland
+    #xdg-desktop-portal-gtk
+    #xdg-desktop-portal-hyprland
+    xwayland
+    waybar
+    (pkgs.waybar.overrideAttrs (oldAttrs: {
+            mesonFlags = oldAttrs.mesonFlags ++ [ "-Dexperimental=true" ];
+          })
+    )
+    dunst # notfications
+    libnotify # for dunst
+    swww # wallpapers
+    kitty # term
+    rofi-wayland # app launcher
+    dolphin # file manager
+    spotify
+  ];
+
+  # # hyprland
+  # services.xserver.videosDrivers = ["nvidia"];
+  # desktop portal
+  #xdg.portal.enable = true;
+  #xdg.portal.extraPortals = [ pkgs.xdg-desktop-portal-gtk ];
+  #xdg.portal.xdgOpenUsePortal = true;
+
+  services.xserver.displayManager.gdm.wayland = true;
+  programs.hyprland = {
+    enable = true;
+    # set the flake package
+    package = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.hyprland;
+    # make sure to also set the portal package, so that they are in sync
+    portalPackage = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.xdg-desktop-portal-hyprland;
+  };
+
+  environment.sessionVariables = {
+    # If your cursor becomes invisible
+    WLR_NO_HARDWARE_CURSORS = "1";
+    # Hint electron apps to use wayland
+    NIXOS_OZONE_WL = "1";
+  };
+  
+  hardware = {
+      graphics.enable = true;
+      # Most wayland compositors need this
+      nvidia.modesetting.enable = true;
+  };
+
+
+
+  # setup flakes
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+
+  # setup home-manager
+  home-manager = {
+    extraSpecialArgs = { inherit inputs; };
+    users = {
+       "nolan" = import ./home.nix;
+    };
+  };
+  # fonts
+  fonts.packages = with pkgs; [
+    font-awesome
+    (nerdfonts.override { fonts = [ "NerdFontsSymbolsOnly" ]; })
+  ];
+
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  # programs.mtr.enable = true;
+  # programs.gnupg.agent = {
+  #   enable = true;
+  #   enableSSHSupport = true;
+  # };
+
+  # List services that you want to enable:
+
+  # Enable the OpenSSH daemon.
+  # services.openssh.enable = true;
+
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  # networking.firewall.enable = false;
+
+  # xkb-layout
+  # services.xserver.displayManager.sessionCommands = "sleep 5 && ${pkgs.xorg.xmodmap}/bin/xmodmap -e 'keycode 43 = h H Left H' &";
+  # services.xserver.displayManager.sessionCommands = "sleep 5 && ${pkgs.xorg.xmodmap}/bin/xmodmap -e 
+  #         'keycode 66 = Escape' &";
+  # services.xserver.displayManager.sessionCommands = ${pkgs.xorg.xmodmap}/bin/xmodmap "${pkgs.writeText  "xkb-layout" ''
+  #       ! Swap escape and capslock
+  #       clear Lock
+  #       keycode 66 = Escape
+  #       !add Lock = Escape
+  #       
+  #       ! Remap Right shift to super
+  #       clear shift
+  #       keycode 62 = Super_L NoSymbol Super_L
+  #       
+  #       ! Exchange left Alt and left Win
+  #       !remove mod1 = Alt_L
+  #       !remove mod4 = Super_L
+  #       !add mod1 = Super_L
+  #       !add mod4 = Alt_L
+  #       
+  #       ! Reset left Alt and left Win to Defaults
+  #       remove mod1 = Super_L
+  #       remove mod4 = Alt_L
+  #       add mod1 = Alt_L
+  #       add mod4 = Super_L
+  #       
+  #       !remove Lock = Caps_Lock
+  #       !keysym Caps_Lock = Escape
+  #       !keysym Escape = Caps_Lock
+  #   ''}"
+  
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "24.05"; # Did you read the comment?
+
+}
