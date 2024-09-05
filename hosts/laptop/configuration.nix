ab555d0b4be4633af9944165740ed0694bbc12d5
NolanBlack <nblackinbox@gmail.com>
2024-09-05 10:35:46 -0600

    56 current  2024-09-05 10:35:37  24.11.20240831.12228ff   6.6.48                          *

--- hosts/laptop/configuration.nix
+++ hosts/laptop/configuration.nix
@@ -98,7 +98,7 @@
 
     # Enable the GNOME Desktop Environment.
     services.xserver.displayManager.gdm.enable = true;
-    services.xserver.desktopManager.gnome.enable = false;
+    services.xserver.desktopManager.gnome.enable = true;
 
     # # hyprland
     # services.xserver.videosDrivers = ["nvidia"];
