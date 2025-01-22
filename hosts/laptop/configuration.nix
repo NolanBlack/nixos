5a5c215c562ede8a75f9f1097c531e4e9867bfa0
nolan <nblackinbox@gmail.com>
2025-01-22 15:54:29 -0700

    20 current  2025-01-22 15:37:38  25.05.20241209.a73246e   6.6.63                          *

--- hosts/laptop/configuration.nix
+++ hosts/laptop/configuration.nix
@@ -134,28 +134,6 @@
         nvidia.modesetting.enable = true;
     };
 
-    # OPENVPN
-    # OPENVPN.optionA run via system
-    # enable openvpn
-    #services.openvpn.servers = {
-    #    officeVPN  = {
-    #        config = '' config /home/nolan/Downloads/vpnconfig_cert.ovpn '';
-    #        updateResolvConf = true;
-    #    };
-    #};
-
-    # OPENVPN.optionB enable systemd-resolved for vpn
-    # run via `sudo openvpn --config path/to/config`
-    # may need to run 
-    #       systemctl enable systemd-resolved.service
-    #       systemctl start systemd-resolved.service
-    services.resolved = {
-        enable = true;
-    };
-
-    # Need to link to libexec to have update-systemd-resolved
-    environment.pathsToLink = [ "/libexec" ];
-
 
 
     ############################################################################
@@ -259,6 +237,27 @@
 
     # Enable the OpenSSH daemon.
     # services.openssh.enable = true;
+    # OPENVPN
+    # OPENVPN.optionA run via system
+    # enable openvpn
+    #services.openvpn.servers = {
+    #    officeVPN  = {
+    #        config = '' config /home/nolan/Downloads/vpnconfig_cert.ovpn '';
+    #        updateResolvConf = true;
+    #    };
+    #};
+
+    # OPENVPN.optionB enable systemd-resolved for vpn
+    # run via `sudo openvpn --config path/to/config`
+    # may need to run 
+    #       systemctl enable systemd-resolved.service
+    #       systemctl start systemd-resolved.service
+    services.resolved = {
+        enable = true;
+    };
+
+    # Need to link to libexec to have update-systemd-resolved
+    environment.pathsToLink = [ "/libexec" ];
 
     # Open ports in the firewall.
     # networking.firewall.allowedTCPPorts = [ ... ];
