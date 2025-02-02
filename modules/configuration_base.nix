02b4de792bc55a45ba0f5c1a35d020c626e3b4f6
nolan <nblackinbox@gmail.com>
2025-02-02 09:30:01 -0700

    22 current  2025-02-02 09:28:24  25.05.20241209.a73246e   6.6.63                          *

--- modules/configuration_base.nix
+++ modules/configuration_base.nix
@@ -1,6 +1,49 @@
 { config, pkgs, inputs, ... }:
 
 {
+    # Bootloader.
+    boot.loader.systemd-boot.enable = true;
+    boot.loader.efi.canTouchEfiVariables = true;
+
+    # Networking
+    networking.hostName = "nixos"; # Define your hostname.
+    # networking.wireless.enable = true;    # Enables wireless support via wpa_supplicant.
+
+    # Configure network proxy if necessary
+    # networking.proxy.default = "http://user:password@proxy:port/";
+    # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+
+    # Enable networking
+    networking.networkmanager.enable = true;
+
+    # System
+    # Set your time zone.
+    time.timeZone = "America/Denver";
+
+    # Select internationalisation properties.
+    i18n.defaultLocale = "en_US.UTF-8";
+
+    i18n.extraLocaleSettings = {
+        LC_ADDRESS = "en_US.UTF-8";
+        LC_IDENTIFICATION = "en_US.UTF-8";
+        LC_MEASUREMENT = "en_US.UTF-8";
+        LC_MONETARY = "en_US.UTF-8";
+        LC_NAME = "en_US.UTF-8";
+        LC_NUMERIC = "en_US.UTF-8";
+        LC_PAPER = "en_US.UTF-8";
+        LC_TELEPHONE = "en_US.UTF-8";
+        LC_TIME = "en_US.UTF-8";
+    };
+
+    # Enable touchpad support (enabled default in most desktopManager).
+    # services.xserver.libinput.enable = true;
+
+    # Allow unfree packages
+    nixpkgs.config.allowUnfree = true;
+
+    # setup flakes
+    nix.settings.experimental-features = [ "nix-command" "flakes" ];
+
     ############################################################################
     # Programs
     ############################################################################
